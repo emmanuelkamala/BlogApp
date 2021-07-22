c6a287587ecf0f68939c3353f606b953cba65128
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-23 01:12:58 +0300

    Create a topbar component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,25 @@ import './topbar.css';
 const Topbar = () => {
   return (
     <div className='top'>
-      
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">Home</li>
+          <li className="topListItem">About</li>
+          <li className="topListItem">Write</li>
+          <li className="topListItem">Contact</li>
+          <li className="topListItem">Logout</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img src="/images/ejoka.png" alt="profile pic" className="topImg" />
+        <i className="topSearch fas fa-search"></i>
+      </div>
     </div>
   )
 }
