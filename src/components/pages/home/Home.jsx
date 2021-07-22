1a6337ba7dfe87cc2acabda63102f94f52c7e5e4
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-23 02:01:49 +0300

    create sidebar component

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import Header from '../../header/Header';
+import Posts from '../../posts/Posts';
+import Sidebar from '../../sidebar/Sidebar';
 import './home.css';
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div>
       <Header />
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
     </div>
   )
 }
