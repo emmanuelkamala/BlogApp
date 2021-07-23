0fa3c5d7a5271d98fa60f2c0e1b350e7b1356841
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-24 01:36:01 +0300

    Create user model

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config()
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+.then(console.log('Connected to MongoDB'))
+.catch(error => console.log(error))
 app.listen(5000, console.log('Server is running'));
---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePic: { type: String, default: ''}
+}, 
+  {timeStamps: true}
+)
+
+module.exports = mongoose.model('User', userSchema);
