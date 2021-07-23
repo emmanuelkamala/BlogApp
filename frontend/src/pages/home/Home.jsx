9a98be91e98f3f5912c7b18f5b8ff5f9a4e2ee6a
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-24 01:19:14 +0300

    Add folder for backend api

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react';
 import Header from '../../components/header/Header';
 import Posts from '../../components/posts/Posts';
 import Sidebar from '../../components/sidebar/Sidebar';
 import './home.css';
 
 const Home = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(()=> {
+    const fetchPosts = async () => {
+      axios.get
+  }})
   return (
     <div>
       <Header />
