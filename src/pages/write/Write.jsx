211c6b768d16b24e5f538c0561cbb14f492ecfd7
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-23 11:56:25 +0300

    Create a write component

---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,24 @@
+import './write.css';
+
+const Write = () => {
+  return (
+    <div className='write'>
+      <img src="/images/beach-banner.jpg" alt="cover" className="writeImg" />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor='fileInput'>
+            <i className='writeIcon fas fa-plus'></i>
+          </label>
+          <input type="file" id="fileInput" style={{display: 'none'}} />
+          <input type="text" placeholder='Enter title' className='writeInput' autoFocus={true} />
+        </div>
+        <div className="writeFormGroup">
+          <textarea placeholder="Tell your story..." type='text' className='writeInput writeText'></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  )
+}
+
+export default Write;
