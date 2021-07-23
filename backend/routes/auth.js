293d7b0bc3893e314cc644af7c800da5a45b7ba6
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-24 01:52:58 +0300

    Create route for user registration

---
+++ backend/routes/auth.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import User from '../models/User';
+
+const router = express.Router();
+
+// REGISTER
+
+router.post('/register', async (req, res) => {
+  try {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    })
+    
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
+
