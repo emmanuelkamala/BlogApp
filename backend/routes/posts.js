ca4a1819364717d2464f9c2ec62d0d1c0e2e65fe
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-27 02:55:02 +0300

    Add route for deleting posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -42,7 +42,25 @@ postRouter.put('/:id', async (req, res) => {
 
 // DELETE 
 
-
+postRouter.put('/:id', async (req, res) => {
+  
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete()
+        res.status(200).json('Post has been deleted ');
+      } catch (error) {
+        res.status(404).json('Post not found');
+      }
+    } else {
+      res.status(401).json('You can only delete your post(s)');
+    }
+  } catch (error) {
+    res.status(500).json({error: error.message });
+  }
+}
+)
 
 
 // GET USER
