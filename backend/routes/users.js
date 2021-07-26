ad0a94135a707e91d4c00981b00bf5df0af445b6
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-07-27 02:15:37 +0300

    Edit update user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,13 +11,13 @@ userRouter.put('/:id', async (req, res) => {
   if (req.body.userId === req.params.id) { 
     if (req.body.password){
       const salt = await bcrypt.genSalt(10);
-      req.body.password = await bcrypt(req.body.password, salt);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
-      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+      await User.findByIdAndUpdate(req.params.id, {
         $set: req.body
-      }, { new: true });
-      res.status(200).json(updatedUser);
+      }, { new: true});
+      res.status(200).json('User has been updated');
     } catch (error) {
       res.status(500).json({error: error.message });
     }
@@ -33,8 +33,8 @@ userRouter.delete('/:id', async (req, res) => {
       const user = await User.findById(req.params.id);
       try {
         await Post.deleteMany({ username: user.username });
-        const deletedUser = await User.findByIdAndDelete(req.params.id);
-        res.status(200).json(deletedUser, 'User has been deleted');
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json('User has been deleted');
       } catch (error) {
         res.status(500).json({error: error.message });
       }
