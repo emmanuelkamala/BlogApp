2f2d089a180d0eb2d97178261bffdc1a905149f9
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-02 16:05:45 +0300

    Create a logout functionality

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -4,7 +4,11 @@ import { Context } from '../../context/Context';
 import './topbar.css';
 
 const Topbar = () => {
-  const {user} = useContext(Context);
+  const { user, dispatch } = useContext(Context);
+
+  const handleLogout = () => {
+    dispatch({ type: 'LOGOUT'})
+  }
   return (
     <div className='top'>
       <div className="topLeft">
@@ -21,12 +25,12 @@ const Topbar = () => {
           <li className="topListItem"><Link to='/about' className='link'>About</Link></li>
           <li className="topListItem"><Link to='/write' className='link'>Write</Link></li>
           <li className="topListItem"><Link to='/contact' className='link'>Contact</Link></li>
-          <li className="topListItem">{user && 'Logout'}</li>
+          <li className="topListItem" onClick={handleLogout}>{user && 'Logout'}</li>
         </ul>
       </div>
       <div className="topRight">
         {
-          user ? (<img src="/images/ejoka.png" alt="profile pic" className="topImg" />) : (
+          user ? (<img src={user.profilePic} alt="profile pic" className="topImg" />) : (
             <ul className='topList'>
               <li className='topListItem'><Link to='/login' className='link'>Login</Link></li>
               <li className='topListItem'><Link to='/register' className='link'>Register</Link></li>
--- frontend/src/context/Action.js
+++ frontend/src/context/Action.js
@@ -10,3 +10,7 @@ export const LoginSuccess = (user) => ({
 export const LoginFailure = () => ({
   type: 'LOGIN FAILURE'
 })
+
+export const Logout = () => ({
+  type: 'LOGOUT',
+})
