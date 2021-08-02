8ef9e8c1de69a1a107ea9f2a39510414a9910676
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-02 09:49:35 +0300

    Add reducer to the app

---
+++ frontend/src/context/Reducer.js
@@ -0,0 +1,29 @@
+const Reducer = (state, action) => {
+  switch(action.type){
+    case 'LOGIN START': 
+      return {
+        user: null,
+        isFetching: true,
+        error: false,
+      }
+
+    case 'LOGIN SUCCESS': 
+      return {
+        user: action.payload,
+        isFetching: false,
+        error: false,
+      }
+    
+    case 'LOGIN FAILURE': 
+      return {
+        user: null,
+        isFetching: false,
+        error: true,
+      }
+
+    default: 
+      return state;
+  }
+}
+
+export default Reducer;
