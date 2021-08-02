084430853b383682fda04ce41a6e445236e62d76
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-02 10:08:55 +0300

    Edit context file

--- frontend/src/context/Context.js
+++ frontend/src/context/Context.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import Reducer from "./Reducer";
 
 const initialState = {
   user: null,
@@ -9,5 +10,17 @@ const initialState = {
 export const Context = createContext(initialState);
 
 export const ContextProvider = ({user}) => {
-  
+  const [state, dispatch] = useReducer(Reducer, initialState)
+
+  return (
+    <Context.Provider 
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+    }}>
+      {children}
+    </Context.Provider>
+  )
 }
